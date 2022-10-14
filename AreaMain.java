63c6de5f8c10bb37c671fd9f991f0476aed1f8dd
Elis Ruiz <earr0119@hotmail.com>
2022-10-14 15:10:59 -0400

    second commit add else statement

--- AreaMain.java
+++ AreaMain.java
@@ -14,6 +14,7 @@ public class AreaMain {
 		System.out.println("==========================================================");
 
 		System.out.println("");
+		
 		do {
 			System.out.print("Please, Type the length: ");
 			double length = in.nextDouble();
@@ -25,14 +26,15 @@ public class AreaMain {
 			returnArea.print();
 
 			count++;
-			// System.out.println(count);
+			//System.out.println(count);
 			System.out.println("Would you like to Keep going: Yes || No");
 			keepGoing = in.next();
 		} while (keepGoing.equalsIgnoreCase("Yes"));
 
 		if (keepGoing.equalsIgnoreCase("No")) {
 			System.out.println("Thank you for creating "+ count +" Areas with our program");
-
+		} else {
+			System.out.println("Error!!! You must type Yes or No");
 		}
 	}
 
